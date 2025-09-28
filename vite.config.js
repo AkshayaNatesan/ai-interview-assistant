9886acd27cdf7bf471ca66727b999d54a19fc616
AkshayaNatesan <152066521+AkshayaNatesan@users.noreply.github.com>
2025-09-28 20:33:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000
+//   }
+// })
+
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // ignore specific warnings
+        if (warning.code === 'UNRESOLVED_IMPORT') return;
+        warn(warning);
+      }
+    }
   }
 })
